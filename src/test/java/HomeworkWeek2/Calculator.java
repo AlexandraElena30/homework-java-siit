902e9cf0471b8d5c43e926d999ae0d99425103de
Elena <alexandra.iordan96@gmail.com>
2020-02-28 17:54:27 +0200

    Homework week 2

---
+++ src/test/java/HomeworkWeek2/Calculator.java
@@ -0,0 +1,33 @@
+package HomeworkWeek2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" x = ");
+        int x = scanner.nextInt();
+        System.out.println(" insert the sign(+,-,*,/,%): ");
+        char  sign = scanner.next().charAt(0);
+        System.out.println(" y = ");
+        int y = scanner.nextInt();
+
+        System.out.print("The results for : "+ x + " " + sign + " " + x + " = ");
+
+        if( sign =='+'){
+            System.out.print(x + y);
+        } else  if( sign =='-') {
+            System.out.print(x - y);
+        } else if( sign =='*') {
+            System.out.print(x * y);
+        } else  if( sign =='/') {
+            System.out.print(x / y);
+        } else  if( sign =='%') {
+            System.out.print(x % y);
+        } else {
+            System.out.println("Incorrect sign! ");
+        }
+
+    }
+}
