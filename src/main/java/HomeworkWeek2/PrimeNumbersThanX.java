40ce31092e5a5bd12f97e49fc88d99d64d58ade5
Elena <alexandra.iordan96@gmail.com>
2020-03-01 10:11:26 +0200

    Homework for week 2 (gradle)

---
+++ src/main/java/HomeworkWeek2/PrimeNumbersThanX.java
@@ -0,0 +1,41 @@
+package HomeworkWeek2;
+
+import java.util.Scanner;
+
+public class PrimeNumbersThanX {
+
+    public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = scanner.nextInt();
+        printPrime(number);
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i < n; i++){
+            if (n % i == 0){
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    static void printPrime(int number) {
+        for (int i = 2; i <= number; i++) {
+            if (isPrime(i))
+                System.out.print(i + " ");
+        }
+    }
+}
+
+
+
+
+
+
+
