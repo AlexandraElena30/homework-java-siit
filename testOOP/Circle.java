f4a45ca036817dd3a865eab4036d86ec5d16747e
AlexandraElena30 <55982168+AlexandraElena30@users.noreply.github.com>
2020-04-08 13:15:12 +0300

    For Test OOP

---
+++ testOOP/Circle.java
@@ -0,0 +1,31 @@
+package testOOP;
+
+public class Circle extends Shapes {
+
+    private double radius;
+    private final double PI = Math.PI;
+
+    public Circle(double radius){
+        this.radius=radius;
+
+    }
+
+
+    @Override
+    public double surface() {
+        double A = 0;
+        A = this.PI * Math.pow(this.radius,2);
+        return A;
+    }
+
+    @Override
+    public double perimeter() {
+        double P = 0;
+        P = 2* this.radius * this.PI;
+        return P;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
---
+++ testOOP/Triangle.java
@@ -0,0 +1,41 @@
+package testOOP;
+
+public class Triangle extends Shapes {
+
+    private double b; //base
+    private double a; //side
+    private double c; //side
+
+    public Triangle(double b, double a, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    @Override
+    public double surface() {
+        double A = 0;
+        //sp = semi perimeter
+        double sp = (this.a + this.b + this.c) / 2;
+        A = Math.sqrt(sp *(sp - this.a)*(sp - this.b)*(sp - this.c));
+        return A;
+    }
+
+    @Override
+    public double perimeter() {
+        double P = 0;
+        P = this.a + this.b + this.c;
+        return P;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getC() {
+        return c;
+    }
+}
