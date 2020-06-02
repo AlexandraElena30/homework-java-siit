02b89adf14281d97d117292dea7bf5e25aa81f00
AlexandraElena30 <55982168+AlexandraElena30@users.noreply.github.com>
2020-06-02 15:20:35 +0300

    Delete Party.java

--- com/siit/homeworks/homework11/exercise05/github_exercises/streams/tutorial/exercise2/Party.java
+++
@@ -1,9 +0,0 @@
-package com.siit.homeworks.homework11.exercise05.github_exercises.streams.tutorial.exercise2;
-
-public enum Party {
-    CONSERVATIVE,
-    LABOUR,
-    LIBERAL_DEMOCRATS,
-    GREEN_PARTY,
-    MONSTER_RAVING_LOONY_PARTY
-}
